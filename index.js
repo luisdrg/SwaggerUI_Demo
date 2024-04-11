c754f10d04002d66c2228716af4c1217402f0ffd
luisdrg <luisdrg@email.unc.edu>
2024-04-11 11:23:37 -0400

    Implement swagger UI with express

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 const express = require('express');
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require ("swagger-jsdoc");
+
+//  Outlines the API's information, detailing its features and where to locate it.
+const options = {
+	definition: {
+		// Specifies the OpenAPI specification version being used.
+		openapi: "3.0.0", 
+		info: {
+			// The title of the API.
+			title: "Library API", 
+			// The version of the API.
+			version: "1.0.0", 
+			// A short description of the API.
+			description: "A simple Express Library API",
+		},
+		// The servers array lists all the servers where the API can be accessed.
+		servers: [
+			{
+				// The URL of the server where the API is hosted.
+				url: "http://localhost:3000",
+			},
+		],
+	},
+	// The paths to the files containing the API endpoints.
+	apis: ["./routes/*.js"], 
+};
+
+// returns the specs needed to setup the UI
+const specs = swaggerJsDoc(options);
 const app = express();
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(specs));
+
 const PORT = 3000;
 const usersRouter = require('./routes/users');
 
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,38 @@
 const express = require('express');
 const router = express.Router();
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - id
+ *         - name
+ *       properties:
+ *         id:
+ *           type: number
+ *           description: The auto-generated id of the user
+ *         name:
+ *           type: string
+ *           description: The user name
+ *         email:
+ *           type: string
+ *           description: The user email
+ *       example:
+ *         id: 787
+ *         name: Hector
+ *         email: example@swagger.com
+ */
+
+//This is under the s
+ /**
+  * @openapi
+  * tags:
+  *   name: Users
+  *   description: The users managing API
+  */
+
 
 // Imagine is coming from a database and is not hardcoded
 let users = [
@@ -10,11 +43,51 @@ let users = [
     { id: 5, name: "Eve", email: "eve@example.com" },
 ];
 
+/**
+ * @openapi
+ * /users:
+ *   get:
+ *     summary: Returns the list of all the users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: The list of the users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
 // Get all users
 router.get('/', (req, res) => {
     res.json(users);
 });
 
+
+/**
+ * @openapi
+ * /users/{id}:
+ *   get:
+ *     summary: Get the user by id
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *         required: true
+ *         description: The user id
+ *     responses:
+ *       200:
+ *         description: The user description by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       404:
+ *         description: The user was not found
+ */
 // Get a single user by id
 router.get('/:id', (req, res) => {
     const user = users.find(u => u.id === parseInt(req.params.id));
@@ -22,6 +95,38 @@ router.get('/:id', (req, res) => {
     res.json(user);
 });
 
+/**
+ * @openapi
+ * /users:
+ *   post:
+ *     summary: Creates a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The user's name
+ *               email:
+ *                 type: string
+ *                 description: The user's email
+ *             example:
+ *               name: NottaRef
+ *               email: not.doe@example.com
+ *     responses:
+ *       201:
+ *         description: The user was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Bad request, name and email are required
+ */
 // Create a new user
 router.post('/', (req, res) => {
     const { name, email } = req.body;
@@ -36,6 +141,47 @@ router.post('/', (req, res) => {
     res.status(201).send(user);
 });
 
+
+
+/**
+ * @openapi
+ * /users/{id}:
+ *   put:
+ *     summary: Updates a user
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The user ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The user's name
+ *               email:
+ *                 type: string
+ *                 description: The user's email
+ *             example:
+ *               name: nottaRefAgain
+ *               email: not2.doe@example.com
+ *     responses:
+ *       200:
+ *         description: The user was successfully updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       404:
+ *         description: The user with the given ID was not found
+ */
 // Update a user
 router.put('/:id', (req, res) => {
     const user = users.find(u => u.id === parseInt(req.params.id));
@@ -48,6 +194,31 @@ router.put('/:id', (req, res) => {
     res.send(user);
 });
 
+
+
+/**
+ * @openapi
+ * /users/{id}:
+ *   delete:
+ *     summary: Deletes a user
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *         required: true
+ *         description: The user ID
+ *     responses:
+ *       200:
+ *         description: The user was successfully deleted
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       404:
+ *         description: The user with the given ID was not found
+ */
 // Delete a user
 router.delete('/:id', (req, res) => {
     const userIndex = users.findIndex(u => u.id === parseInt(req.params.id));
